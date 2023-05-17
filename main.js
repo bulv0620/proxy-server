880c42f17a8e50e42edca4251a4be49494e48b83
bulv0620 <1241278815@qq.com>
2023-05-17 21:06:10 +0800

    init

---
+++ main.js
@@ -0,0 +1,64 @@
+const { app, BrowserWindow, Menu, ipcMain, Tray } = require("electron");
+const path = require("path");
+const express = require("express");
+const cors = require('cors')
+const proxy = require("http-proxy-middleware").createProxyMiddleware;
+
+Menu.setApplicationMenu(null);
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 200,
+    show: false,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    frame: false,
+  });
+
+  win.loadFile("index.html");
+
+  win.once("ready-to-show", () => {
+    win.show();
+  });
+
+  const server = express();
+  server.use(cors())
+  server.use("/", proxy(`/productScan`, { target: `http://localhost:55555/productScan` }));
+  const serverInstance = server.listen(3000, () => {
+    win.webContents.send("ready", 0);
+  });
+
+  ipcMain.on("hide", () => {
+    win.hide();
+  });
+
+  const tray = new Tray(path.resolve(__dirname, "./favicon.ico"));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "打开",
+      click: () => {
+        win.show();
+      },
+    },
+    {
+      label: "关闭",
+      click: () => {
+        serverInstance.close();
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("proxy server");
+  tray.setContextMenu(contextMenu);
+  tray.addListener("double-click", () => {
+    win.show();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
